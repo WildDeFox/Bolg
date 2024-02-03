a3774c7b612cef85053a3b7ababc1969540f6827
defox <wildmrdefox@gmail.com>
2024-02-03 18:23:47 +0700

    Добавление интерфейса репозитория пользователя

---
+++ src/Blog/Repositories/UserRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Blog\Defox\Blog\Repositories;
+
+use Blog\Defox\Blog\User;
+use Blog\Defox\Blog\UUID;
+
+interface UserRepositoryInterface
+{
+    public function save(User $user): void;
+    public function get(UUID $uuid): User;
+    public function getByUsername(string $username): User;
+}
